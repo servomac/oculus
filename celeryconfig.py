06c1a1c1a6d08c3b6ef920bfa4d5d4cc2bdcd618
Toni Pizà <tpiza@habitissimo.com>
2015-05-02 11:54:41 +0200

    celery tasks for retrieving data

---
+++ celeryconfig.py
@@ -0,0 +1,18 @@
+from celery.schedules import crontab
+
+CELERYBEAT_SCHEDULE = {
+    'memory': {
+        'task': 'tasks.poll',
+        'schedule': crontab(minute='*/1'),
+        'args': ('mem',),
+    },
+    'cpu': {
+        'task': 'tasks.poll',
+        'schedule': crontab(minute='*/1'),
+        'args': ('cpu',),
+    },
+}
+
+# Change this to your settings
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
---
+++ tasks.py
@@ -0,0 +1,40 @@
+import json
+import redis
+import requests
+from celery import Celery
+from datetime import datetime
+
+from settings import container_id, API_URL
+from settings import REDIS_HOST, REDIS_PORT, REDIS_DB
+from settings import REDIS_KEY, REDIS_KEY_TIMESTAMP, REDIS_EXPIRE_TIME
+
+celery = Celery('tasks')
+celery.config_from_object('celeryconfig')
+
+
+def construct_request(api_url, container_id, dest):
+    return '{api}/containers/{container}/{dest}'.format(api=api_url,
+                                          container=container_id,
+                                          dest=dest)
+
+@celery.task
+def poll(resource):
+    req_url = construct_request(API_URL, container_id, resource)
+    req = requests.get(req_url)
+
+    if req.status_code != 200:
+        raise Exception('Bad status code')
+
+    if req.text == '-1' or req.text == 'id not found':
+        raise Exception('Unknown container {} [{}]'.format(container_id, req.text))
+
+    # save the results polled from the API to redis
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+    key = REDIS_KEY.format(timestamp=datetime.now().strftime(REDIS_KEY_TIMESTAMP),
+                           container_id=container_id, resource=resource)
+    print "{}: {}".format(key, int(req.text))
+    if resource == 'net':
+        pass
+    else:
+        r.set(key, int(req.text))
+        r.expire(key, REDIS_EXPIRE_TIME)
