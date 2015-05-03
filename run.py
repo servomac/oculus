4eed17a2417674f6644a35fd289be01833bbdb85
Toni Pizà <tpiza@habitissimo.com>
2015-05-03 18:49:01 +0200

    redis pool connection and a retrieve function. I start to feel the need to encapsulate Redis connections in a different class..

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import redis
 import requests
+import time
 
 from datetime import datetime, timedelta
 from docker import Client
@@ -11,6 +12,48 @@ from settings import REDIS_HOST, REDIS_PORT, REDIS_DB
 from settings import REDIS_KEY, REDIS_KEY_TIMESTAMP
 
 app = Flask(__name__)
+redis_pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000  # millisecs
+        if self.verbose:
+            print 'elapsed time: %f ms' % self.msecs
+
+
+def retrieve(container_id, resource, instant):
+    """ Obtain the data stored in Redis.
+        Keyword arguments:
+         - container_id: the desired container_id
+         - resource: 'cpu', 'mem' or 'net'
+         - instant: an specific datetime
+         - r: redis connection
+    """
+    r = redis.Redis(connection_pool=redis_pool)
+    key = REDIS_KEY.format(timestamp=instant.strftime(REDIS_KEY_TIMESTAMP),
+                           container_id=container_id,
+                           resource=resource)
+    value = r.get(key)
+
+    if resource == 'mem':
+        # convert bytes to MB (memory usage)
+        value = int(value) / (1024.0)**2
+    elif resource == 'net':
+        # return a dictionary
+        value = loads(value)
+
+    return value
+
+
 
 @app.route('/')
 def main_page():
@@ -31,21 +74,16 @@ def describe_container(container_id):
     data['time'] = []
 
     # obtain timeseries from redis
-    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
-    for resource in ['cpu', 'mem', 'net']:
-        data[resource] = []
-        for delta in range(60):
-            timestamp = datetime.now() - timedelta(minutes=delta)
-            key = REDIS_KEY.format(timestamp=timestamp.strftime(REDIS_KEY_TIMESTAMP),
-                                   container_id=container_id, resource=resource)
-            value = r.get(key)
-            value = loads(value) if resource == 'net' else value
-            if value != None:
-                data[resource].append(value)
-                data['time'].append(timestamp.strftime('%Y-%m-%d %H:%M'))
+    with Timer(verbose=True) as t:
+        for resource in ['cpu', 'mem', 'net']:
+            data[resource] = []
+            for delta in range(60):
+                timestamp = datetime.now() - timedelta(minutes=delta)
+                value = retrieve(container_id, resource, timestamp)
+                if value != None:
+                    data[resource].append(value)
+                    data['time'].append(timestamp.strftime('%Y-%m-%d %H:%M'))
 
-    # convert bytes to MB (memory usage)
-    data['mem'] = [int(x) / (1024.0)**2 for x in data['mem']]
     # construct lists from list of jsons (network)
     data['net'] = {
         'tx': [x['TxBps'] for x in data['net']],
